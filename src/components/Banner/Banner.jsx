e07b02a2ee9bc9874e0286e35d3c48dbc29b320a
Kallol Dey <kalloldey067@gmail.com>
2026-03-30 10:58:38 +0600

    rating section done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { PiLineVerticalThin } from 'react-icons/pi';
 import { VscDebugStart } from 'react-icons/vsc';
 
 const Banner = () => {
     return (
-        <div className='flex gap-35 p-35'>
-            <div className='left space-y-5 my-auto'>
+        <div>
+            <div className='flex gap-35 p-35'>
+                <div className='left space-y-5 my-auto'>
                 <div className='bg-blue-100 flex items-center pl-2 w-70 p-1 rounded-4xl gap-2'><img src="/src/assets/Group 5.png" alt="" /><p className='bg-linear-to-r from-purple-800 to-purple-600 bg-clip-text text-transparent'>New: AI-Powered Tools Available</p></div>
                 <h1 className='text-7xl font-bold'>Supercharge Your <br />Digital Workflow</h1>
                 <p>Access premium AI tools, design assets, templates, and productivity <br />
@@ -19,7 +21,26 @@ const Banner = () => {
             <div className="right">
                 <img src="/src/assets/banner.png" alt="" />
             </div>
+            </div>
+
+            <div className='flex p-15 justify-evenly items-center bg-linear-to-r from-purple-800 to-purple-600'>
+                <div className='space-y-2'>
+                    <h2 className='text-5xl font-bold text-white'>50K+</h2>
+                    <p className='text-white'>Active Users</p>
+                </div>
+                <PiLineVerticalThin className='text-7xl text-white opacity-50' />
+                <div className='space-y-2'>
+                    <h2 className='text-5xl font-bold text-white'>200+</h2>
+                    <p className='text-white'>Premium Tools</p>
+                </div>
+                <PiLineVerticalThin className='text-7xl text-white opacity-50' />
+                <div className='space-y-2'>
+                    <h2 className='text-5xl font-bold text-white'>4.9</h2>
+                    <p className='text-white'>Rating</p>
+                </div>
+            </div>
         </div>
+        
     );
 };
 
